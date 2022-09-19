8c59cab9cd04e81e8506b4e72182692969fd723e
tluwaniMS <tluwani2020@gmail.com>
2022-09-19 15:14:15 +0200

    sonar-linting-fix

--- services/mutation-services.js
+++ services/mutation-services.js
@@ -1,7 +1,7 @@
 const { users } = require("../sample-data/sample-users-data");
 
 const createUser = (parent, args) => {
-  user = { firstName: args.firstName, lastName: args.lastName, email: args.email };
+  let user = { firstName: args.firstName, lastName: args.lastName, email: args.email };
   users.push(user);
   return `User with name ${user.firstName} has been created succesfully`;
 };
